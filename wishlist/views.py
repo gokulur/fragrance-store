0ba873c188df044267cdb2e798873d6ed17f6a66
GOKUL UR <gokulkrishnagokul6@gmail.com>
2025-12-10 17:50:17 +0530

    wishlist views updated

--- wishlist/views.py
+++ wishlist/views.py
@@ -7,8 +7,14 @@ from django.views.decorators.http import require_POST
 
  
 def wishlist_page(request):
-    wishlist, created = Wishlist.objects.get_or_create(user=request.user)
-    products = wishlist.products.all()
+
+    if request.user.is_authenticated:
+        wishlist, created = Wishlist.objects.get_or_create(user=request.user)
+        products = wishlist.products.all()
+    else:
+        session_wishlist = request.session.get("wishlist", [])
+        products = Product.objects.filter(id__in=session_wishlist)
+
     return render(request, 'wishlist.html', {
         'products': products
     })
@@ -26,22 +32,64 @@ def wishlist_page(request):
 
 
 
-@login_required
-@require_POST
+# @login_required
+# @require_POST
+# def toggle_wishlist(request, product_id):
+#     product = get_object_or_404(Product, id=product_id)
+#     wishlist, created = Wishlist.objects.get_or_create(user=request.user)
+
+#     if product in wishlist.products.all():
+#         wishlist.products.remove(product)
+#         added = False
+#     else:
+#         wishlist.products.add(product)
+#         added = True
+
+#     return JsonResponse({
+#         "added": added,
+#         "wishlist_count": request.user.wishlist.products.count()
+#     })
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from .models import Product, Wishlist
+
 def toggle_wishlist(request, product_id):
     product = get_object_or_404(Product, id=product_id)
-    wishlist, created = Wishlist.objects.get_or_create(user=request.user)
 
-    if product in wishlist.products.all():
-        wishlist.products.remove(product)
-        added = False
+    # ----------------------------------------
+    # If user is logged in
+    # ----------------------------------------
+    if request.user.is_authenticated:
+        wishlist, created = Wishlist.objects.get_or_create(user=request.user)
+        if product in wishlist.products.all():
+            wishlist.products.remove(product)
+            added = False
+        else:
+            wishlist.products.add(product)
+            added = True
+        count = wishlist.products.count()
+    
+    # ----------------------------------------
+    # Guest user - store wishlist in session
+    # ----------------------------------------
     else:
-        wishlist.products.add(product)
-        added = True
+        session_wishlist = request.session.get('wishlist', [])
+        product_id_str = str(product.id)
+
+        if product_id_str in session_wishlist:
+            session_wishlist.remove(product_id_str)
+            added = False
+        else:
+            session_wishlist.append(product_id_str)
+            added = True
+
+        request.session['wishlist'] = session_wishlist
+        request.session.modified = True
+        count = len(session_wishlist)
 
     return JsonResponse({
         "added": added,
-        "wishlist_count": request.user.wishlist.products.count()
+        "wishlist_count": count
     })
 
 
