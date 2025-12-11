d8d1bbdad2c6530249b80b2be360f5569fad5876
GOKUL UR <gokulkrishnagokul6@gmail.com>
2025-12-12 02:26:03 +0530

    admin order updated

--- orders/models.py
+++ orders/models.py
@@ -36,6 +36,10 @@ class Order(models.Model):
 
     def __str__(self):
         return self.order_id
+    
+    @property
+    def total_quantity(self):
+        return sum(item.quantity for item in self.items.all())
 
 
 
@@ -55,6 +59,9 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return f"{self.product} x {self.quantity}"
+    
+    
+
 
 
 class ShippingAddress(models.Model):
